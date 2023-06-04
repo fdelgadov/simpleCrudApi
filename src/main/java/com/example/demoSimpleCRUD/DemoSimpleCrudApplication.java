3a6e1d3dc918eedcfa91329c3a608d3ab03c8763
fdelgadov <fdelgadov@unsa.edu.pe>
2023-06-03 22:33:29 -0500

    Creación de proyecto de spring boot

---
+++ src/main/java/com/example/demoSimpleCRUD/DemoSimpleCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoSimpleCRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSimpleCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSimpleCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoSimpleCRUD/DemoSimpleCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoSimpleCRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSimpleCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
